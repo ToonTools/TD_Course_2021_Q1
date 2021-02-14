6c2acfaed6088de6a64f27db07ebf336e8280a57
chrishcarter <chrishcarter@gmail.com>
2021-02-14 20:09:42 +0000

    Give user choice on adding lines

--- TD_3_createTextFile.js
+++ TD_3_createTextFile.js
@@ -15,16 +15,26 @@ function makeTextFile(){
 	MessageLog.trace("make button pressed")
 
 	var myFile = new File(fileCompletePath)
-
 	if (myFile.exists){
 		//if true do this
 		MessageLog.trace("file already exists")
+		var message 	= "file exists, do you want to add a line to it?"
+		var ans 		= MessageBox.warning(message,MessageBox.Yes, MessageBox.No)
 
+		if(ans !=MessageBox.Yes){
+			return false
+		}
+		else{
+			myFile.open(FileAccess.Append)
+			myFile.writeLine("this is a new line")
+			myFile.close()
+			MessageLog.trace("line was added to the text file")
+		}
 	}
 	else{
 		//if false do this
 		MessageLog.trace("file does not exist so I will make a new one")
-		myFile.open(FileAccess.Append)
+		myFile.open(FileAccess.WriteOnly )
 		myFile.close()
 		
 	}
@@ -32,8 +42,4 @@ function makeTextFile(){
 
 function readTextFile(){
 	MessageLog.trace("read button pressed")
-
-
-
-
 }
