6dacc599e46c8cddf6ac53253f1d5bfd03b64929
chrishcarter <chrishcarter@gmail.com>
2021-02-14 19:54:25 +0000

    Makes file if file does not already exist

--- TD_3_createTextFile.js
+++ TD_3_createTextFile.js
@@ -9,13 +9,31 @@ var myPath 		= "C:\\Users\\" + userName + "\\Desktop\\"
 var fileName	= "myTextFile.txt"
 var fileCompletePath = myPath + fileName
 
-MessageLog.trace(fileCompletePath)
-
+//MessageLog.trace(fileCompletePath)
 
 function makeTextFile(){
 	MessageLog.trace("make button pressed")
+
+	var myFile = new File(fileCompletePath)
+
+	if (myFile.exists){
+		//if true do this
+		MessageLog.trace("file already exists")
+
+	}
+	else{
+		//if false do this
+		MessageLog.trace("file does not exist so I will make a new one")
+		myFile.open(FileAccess.Append)
+		myFile.close()
+		
+	}
 }
 
 function readTextFile(){
 	MessageLog.trace("read button pressed")
+
+
+
+
 }
