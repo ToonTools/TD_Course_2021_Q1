59c6c8270b1c1d753fde2fca320da72969529e0c
chrishcarter <chrishcarter@gmail.com>
2021-02-14 19:08:17 +0000

    Update TD_2_sceneInformation.js

--- TD_2_sceneInformation.js
+++ TD_2_sceneInformation.js
@@ -7,13 +7,11 @@ function sceneInformation(){
 
 
 	// tell me the number of nodes selected
-	var mySelection 		= selection.selectedNodes()
-	var mySelectionLength 	= mySelection.length
-	
+	var mySelection =   selection.selectedNodes()
+	var mySelectionLength = mySelection.length
+
 	// print out all of the nodes selected in a list
 
-	MessageLog.trace("Scene name = " + sceneName )
-
-	MessageLog.trace( "I have "+ mySelectionLength +" nodes selected.\nMy Selection = " + mySelection)
+	MessageLog.trace("\nScene name = " + sceneName +  "\nI have "+ mySelectionLength +" nodes selected.\nMy Selection = " + mySelection)
 
 }
