8a888bf78518ce961310333717d7a8607305788b
chrishcarter <chrishcarter@gmail.com>
2021-02-14 19:32:34 +0000

    Update TD_3_createTextFile.js

--- TD_3_createTextFile.js
+++ TD_3_createTextFile.js
@@ -1,3 +1,17 @@
+
+// get username
+// text file location
+// text file name
+// have a text file
+
+var userName 	= about.userName
+var myPath 		= "C:\\Users\\" + userName + "\\Desktop\\"
+var fileName	= "myTextFile.txt"
+var fileCompletePath = myPath + fileName
+
+MessageLog.trace(fileCompletePath)
+
+
 function makeTextFile(){
 	MessageLog.trace("make button pressed")
 }
