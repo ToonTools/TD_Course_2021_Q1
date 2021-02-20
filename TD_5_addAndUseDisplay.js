b9cfbe25b33a3fd62282f96d013bacbce044c655
chrishcarter <chrishcarter@gmail.com>
2021-02-20 19:17:25 +0000

    only allows single node selection

--- TD_5_addAndUseDisplay.js
+++ TD_5_addAndUseDisplay.js
@@ -7,11 +7,20 @@
 function addAndUseDiplay(){
     MessageLog.trace("addAndUseDisplays clicked")
 
+    var mySelection = selection.selectedNodes()
+
+    if( mySelection.length != 1){
+        MessageLog.trace("sorry, im only handling 1 node at the moment")
+        return
+    }
+    else{
+        MessageLog.trace("node selected = " + mySelection[0])
+    }
+
+
     node.add("Top" , "myComposite" , "COMPOSITE" , 0, 0, 0)
 
-
-
-}
+} 
 
 
 
