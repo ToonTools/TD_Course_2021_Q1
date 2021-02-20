664359ae0f4a43b59b29770cf5431e84ffb4506f
chrishcarter <chrishcarter@gmail.com>
2021-02-20 19:12:01 +0000

    Create TD_5_addAndUseDisplay.js

---
+++ TD_5_addAndUseDisplay.js
@@ -0,0 +1,34 @@
+
+// Step 1----------
+// get user selection
+// create composite node
+// connect selection to composite node
+
+function addAndUseDiplay(){
+    MessageLog.trace("addAndUseDisplays clicked")
+
+    node.add("Top" , "myComposite" , "COMPOSITE" , 0, 0, 0
+
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// Step 2 ---------
+// create display
+// connect composite to display
+// make display the current display
