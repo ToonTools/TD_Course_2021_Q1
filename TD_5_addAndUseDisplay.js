21bec4f9cdc44abc590453794ebc92fa2550a72b
chrishcarter <chrishcarter@gmail.com>
2021-02-20 19:29:05 +0000

    multiple input nodes will connect to composite

--- TD_5_addAndUseDisplay.js
+++ TD_5_addAndUseDisplay.js
@@ -8,8 +8,21 @@ function addAndUseDiplay(){
     MessageLog.trace("addAndUseDisplays clicked")
 
     var mySelection     = selection.selectedNodes()
+    
+    if( mySelection.length == 0){
+        MessageLog.trace("nothing selected, nothing to do...")
+        return
+    }
+
     var myNewComposite  = node.add("Top" , "myComposite" , "COMPOSITE" , 0, 0, 0)
 
+    for( i = 0 ; i < mySelection.length ; i++){
+        var selNode = mySelection[i]
+        node.link(selNode, 0 , myNewComposite, i, false, true)
+        //MessageLog.trace(selNode)
+    }
+
+    /*
     if( mySelection.length != 1){
         MessageLog.trace("sorry, im only handling 1 node at the moment")
         return
@@ -17,11 +30,16 @@ function addAndUseDiplay(){
     else{
         MessageLog.trace("node selected = " + mySelection[0])
 
-
         node.link(mySelection[0], 0 , myNewComposite, 0, false, true);
     }
+    */
 } 
 
+
+
+// make composite near the source nodes
+//  
+// make it handle more than one item selected
 
 
 
