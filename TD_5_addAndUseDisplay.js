d710c707b3d37a3447b4e47dc4459c48d43f8748
chrishcarter <chrishcarter@gmail.com>
2021-02-20 19:42:20 +0000

    will check against invalid types

--- TD_5_addAndUseDisplay.js
+++ TD_5_addAndUseDisplay.js
@@ -7,48 +7,41 @@
 function addAndUseDiplay(){
     MessageLog.trace("addAndUseDisplays clicked")
 
-    var mySelection     = selection.selectedNodes()
+    var mySelection         = selection.selectedNodes()
+    var invalidTypes        = ["PEG","DISPLAY"]
     
     if( mySelection.length == 0){
         MessageLog.trace("nothing selected, nothing to do...")
         return
     }
+    for( i = 0 ; i < mySelection.length ; i++){
+        // is this node a type that can be connected to a composite?
+        // if any are not then return 
+        var nodeName    = mySelection[i]
+        var selType     = node.type(nodeName)     
+        
+        for( invalid_intex in invalidTypes ){
+            var this_invalidType = invalidTypes[invalid_intex]
+            if( selType == this_invalidType ){
+                MessageLog.trace("at least one of the selected types can not be connected to a composite")
+                return 
+            }
+        }
+    }
 
     var myNewComposite  = node.add("Top" , "myComposite" , "COMPOSITE" , 0, 0, 0)
 
-    for( i = 0 ; i < mySelection.length ; i++){
-        var selNode = mySelection[i]
-        node.link(selNode, 0 , myNewComposite, i, false, true)
+    for( j = 0 ; j < mySelection.length ; j++){
+        var selNode = mySelection[j]
+        node.link(selNode, 0 , myNewComposite, j, false, true)
         //MessageLog.trace(selNode)
     }
-
-    /*
-    if( mySelection.length != 1){
-        MessageLog.trace("sorry, im only handling 1 node at the moment")
-        return
-    }
-    else{
-        MessageLog.trace("node selected = " + mySelection[0])
-
-        node.link(mySelection[0], 0 , myNewComposite, 0, false, true);
-    }
-    */
 } 
 
 
 
 // make composite near the source nodes
-//  
-// make it handle more than one item selected
-
-
-
-
-
-
-
-
-
+// validate selected nodes
 
 
 
