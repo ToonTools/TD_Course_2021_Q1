1f3caf715c7c366a274c59e27548c5d62885fce0
chrishcarter <chrishcarter@gmail.com>
2021-02-27 18:04:34 +0000

    now working

--- TD_9_customView/configure.js
+++ TD_9_customView/configure.js
@@ -1,18 +1,15 @@
-function configure(packageFolder, packageName){
-    if(about.isStageEssentials() || about.isStageAdvanced() || about.isPaintMode()){
-        return
-    } 
-    ScriptManager.addView({
-                            id:"TD_9_customView",
-                            text: translator.tr("TD 9 customView"),
-                            action: "createView in ./configure.js"
-    })
-}
+function configure(packageFolder, packageName)
+{
+  if(about.isStageEssentials() || about.isStageAdvanced() || about.isPaintMode())
+    return;
 
+  ScriptManager.addView( { id : "TD_9_customView",
+                           text : translator.tr("TD_9_customView"),
+                           action : "createView in ./configure.js"} );
+}
 function createView()
 {
-    var view = require("./view.js")
-    view.initView();
+  var view = require("./view.js");
+  view.initView();
 }
-
 exports.configure = configure;
--- TD_9_customView/view.js
+++ TD_9_customView/view.js
@@ -1,30 +1,30 @@
-var uiUtilLib = require(specialFolders.resource+"/scripts/utilities/ui/TB_UiUtils.js")
+var uiUtilLib    = require(specialFolders.resource+"/scripts/utilities/ui/TB_UiUtil.js");
 
 function initView()
 {
-    try{
-        var scriptedView = ScriptManager.getView("TD_9_customView")
-        scriptedView.minimumWidth = UiLoader.dpiScale(375)
-        scriptedView.setPreferredLayoutSize(UiLoader.dpiScale(320), (UiLoader.dpiScale(380)))
+  try
+  {
+    var scriptedView = ScriptManager.getView("TD_9_customView");
+    scriptedView.minimumWidth = UiLoader.dpiScale(375);
+    scriptedView.setPreferredLayoutSize(UiLoader.dpiScale(320), UiLoader.dpiScale(380));
+    
+    var ui = ScriptManager.loadViewUI(scriptedView, "./TD_9_customView.ui");
+   
+    var customFunction = function(){
 
-        var ui = ScriptManger.loadViewUI(scriptedView, "./TD_9_customView.ui")
+      MessageBox.information("This is the action for button1 !");
 
-        var customFunction = function (){
-
-            MessageBox.information("This is the action for button1 !")
-
-        }
-
-        ui.button1.clicked(this, customFunction )
-
-        ui.show()
     }
 
-    catch(e)
-    {
-        MessageLog.trace("Error : " + e )
-        print("Error : " + e)
-    }
+    ui.button1.clicked.connect(this, customFunction);
+
+    ui.show();
+  }
+  catch(e)
+  {
+    MessageLog.trace("Error: " + e);
+    print("Error: " + e);
+  }
 }
 
 exports.initView = initView;
