93690438e61a5d491ea843d7ff921bbc0f18bbf3
chrishcarter <chrishcarter@gmail.com>
2021-03-06 19:02:29 +0000

    really fixed mysterious syntax error

--- TD_10_scriptedMasterController/TD_10_scriptedMasterController.js
+++ TD_10_scriptedMasterController/TD_10_scriptedMasterController.js
@@ -1,32 +1,28 @@
-// script to be called by "scripted_masterController" node
+Controller.onShowControl = function(){  
 
-// when the controller is shown what should I do?
-Controller.onShowControl = function(){
-
-    Controller.controls = []
-    var valAttr 		= node.getAttr(Controller.node, frame.current(), "checkbox_value");
-    var checkBox        = new CheckboxWidget({
+    Controller.controls = [];
+   var valAttr = node.getAttr(Controller.node, frame.current(), "checkbox_value");
+   var checkBox = new  CheckboxWidget({ 
 
         data: valAttr,
-            position : Point2D(0.,0.),
-            screen_space : false,
-            outer_color : ColorRGBA(0,0,0,255),
-            on_color : ColorRGBA(0,255,0,127),
-            off_color : ColorRGBA(255,0,0,127),
-            size : 1
+           position : Point2d(0.,0.),
+           screen_space : false,
+           outer_color : ColorRGBA(0,0,0,255),
+           on_color : ColorRGBA(0,255,0,128),
+           off_color : ColorRGBA(255,0,0,128),
+           size : 1
+   });
 
-    } );
-
-    checkBox.valueChanged.connect(toggleValue)
-    Controller.controls = [checkBox]
+   checkBox.valueChanged.connect(toggleValue);
+ 
+     Controller.controls = [checkBox];
 }
 
-// when the controller is hidden what should I do?
-Controller.onHideControl = function(){
-    MessageLog.trace("Master Controller is hidden")
-    node.setTextAttr(Controller.node, "checkboxvalue", frame.current(), false)
-}
+Controller.onHideControl = function(){  
 
+   MessageLog.trace("Master Controller is hidden");
+   node.setTextAttr(Controller.node, "checkboxvalue",  frame.current(), false);
+}
 
 // when value is true what should I do?
 function toggleValue(valAttr){
